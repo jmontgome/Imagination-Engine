f7c442f24759cd98281e3044ba3a90fd038ca124
Jonathon Montgomery <87336298+jmontgome@users.noreply.github.com>
2024-06-26 21:57:52 -0500

    Initial code commit

---
+++ Imagination.Engine/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+	std::string lastname;
+	
+	std::cout << "Hello World" << std::endl;
+	std::cin >> lastname;
+
+	std::cout << "You entered " << lastname << " as your last name.";
+}
